e88214de2c9d5be6ea20636c5d1be91ebc97c43f
obroslakjakub <obroslakjakub@gmail.com>
2024-07-05 01:11:42 +0200

    Add triangle example

--- examples/triangle/triangle_layer.cc
+++ examples/triangle/triangle_layer.cc
@@ -1,7 +1,30 @@
 #include "triangle_layer.hh"
 
-TriangleLayer::TriangleLayer(mge::Scene& scene)
-    : m_scene(scene), m_shader(mge::ShaderSystem::acquire(fs::current_path() / "shaders" / "triangle")) {}
+#include <filesystem>
+namespace fs = std::filesystem;
 
-void TriangleLayer::configure() {}
-void TriangleLayer::update() {}
+TriangleLayer::TriangleLayer()
+    : m_shader(mge::ShaderSystem::acquire(fs::current_path() / "examples" / "triangle" / "shaders" / "triangle")),
+      m_vertex_buffer(std::make_unique<mge::Buffer<TriangleVertex>>()),
+      m_vertex_array(std::make_unique<mge::VertexArray>()) {}
+
+void TriangleLayer::configure() {
+    auto vertices = std::vector<TriangleVertex>{
+        {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},
+        {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
+        {{0.0f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}}};
+    m_vertex_buffer->bind();
+    m_vertex_buffer->copy(vertices);
+    m_vertex_buffer->unbind();
+    m_vertex_array->bind();
+    m_vertex_array->attach_buffer(*m_vertex_buffer.get(), TriangleVertex::get_attributes());
+    m_vertex_array->unbind();
+}
+
+void TriangleLayer::update() {
+    m_shader->bind();
+    m_vertex_array->bind();
+    glDrawArrays(GL_TRIANGLES, 0, m_vertex_buffer->get_count());
+    m_vertex_array->unbind();
+    m_shader->unbind();
+}
--- examples/triangle/triangle_layer.hh
+++ examples/triangle/triangle_layer.hh
@@ -3,9 +3,18 @@
 
 #include "mge.hh"
 
+struct TriangleVertex {
+  glm::vec3 position;
+  glm::vec3 color;
+
+  inline static std::vector<mge::VertexAttribute> get_attributes() {
+    return {{.size = 3u, .type = GL_FLOAT}, {.size = 3u, .type = GL_FLOAT}};
+  }
+};
+
 class TriangleLayer : public mge::Layer {
  public:
-  TriangleLayer(mge::Scene& scene);
+  TriangleLayer();
   ~TriangleLayer() = default;
 
   virtual void configure() override;
@@ -13,8 +22,8 @@ class TriangleLayer : public mge::Layer {
 
  private:
   std::unique_ptr<mge::VertexArray> m_vertex_array = nullptr;
-  std::shared_ptr<mge::ShaderProgram> m_shader;
-  mge::Scene& m_scene;
+  std::unique_ptr<mge::Buffer<TriangleVertex>> m_vertex_buffer = nullptr;
+  std::shared_ptr<mge::ShaderProgram> m_shader = nullptr;
 };
 
 #endif  // MGE_TRIANGLE_LAYER
